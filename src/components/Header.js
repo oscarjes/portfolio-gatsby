d8cc45e455cf2559f23f8b03ce4dd92e16324fa3
oscarjes <oscar@jesionek.co>
2018-04-07 18:19:59 +0700

    new twitter handle

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = (props) => (
                 <h1>Hi, I'm Oscar</h1>
                 <p>Fullstack developer and Growth Marketer</p>
                 <ul className="icons">
-                    <li><a href="https://www.twitter.com/itsoscarjes" target="_blank" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+                    <li><a href="https://www.twitter.com/oscarjesionek" target="_blank" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
                     <li><a href="https://github.com/oscarjes/" target="_blank" className="icon fa-github"><span className="label">GitHub</span></a></li>
                 </ul>
             </div>
